9a32717fe8dc28613ac7d6ffd6b23df74faa1580
Lobna <monemail@mm.com>
2024-03-17 18:24:10 +0100

    Add homepage

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomepageController extends AbstractController
+{
+    #[Route('/', name: 'app_home', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('homepage/index.html.twig', [
+            'controller_name' => 'HomepageController',
+        ]);
+    }
+    #[Route('/home', name: 'app_homepage_alt', methods: ['GET'])]
+    #[Route('/homepage', name: 'app_homepage', methods: ['GET'])]
+    public function homepage(): RedirectResponse
+    {
+        return $this->redirectToRoute('app_home');
+    }
+}
